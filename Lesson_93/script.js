f5429ff371cfe49d7f8ed2ba89b8c29eb36a9947
codershiyar <codershiyar@gmail.com>
2020-06-23 22:24:48 +0200

    update

---
+++ Lesson_93/script.js
@@ -0,0 +1,33 @@
+// Coder Shiyar   -   https://youtube.com/codershiyar
+// -----------------------------------------------------------------------------
+// JSON   JavaScript Object Notation 
+// يتم استخدام جي سون لتبادل البيانات بين المتصفح والخادم 
+// جي سون خفيف الوزن اثناء  تبادل بيانات 
+
+var json = {"name":"Coder Shiyar" ,
+ "age": 20,
+ "skilles":["HTML","CSS"],
+ "data":{name:"shiyar" , password:"1234"}
+
+};
+
+console.log(json.data.password);
+
+
+
+
+
+  // JSON Values
+// In JSON, values must be one of the following data types:
+
+// a string
+// a number
+// an object (JSON object)
+// an array
+// a boolean
+// null
+// In JavaScript values can be all of the above, plus any other valid JavaScript expression, including:
+
+// a function
+// a date
+// undefined
